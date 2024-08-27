cdeff587e978361a63aac49e051343ad174f1a79
entr0pia <u0jglqhh@duck.com>
2024-08-27 22:39:45 +0800

    对齐

---
+++ magick.ps1
@@ -0,0 +1,10 @@
+$start = 62
+
+for ($i = 0; $i -lt 7; $i++) { 
+    $n = ($start + $i).ToString("D4")
+    $name = "$n.png"
+    # echo $name
+    Magick $name -bordercolor none -border 0x1 $name
+    Magick $name -gravity South -chop 0x1 $name
+    # Magick $name -gravity East  -chop 1x0 $name
+}
