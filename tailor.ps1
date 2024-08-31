9491df4af64656a065407c2f823a14439bd6fa43
entr0pia <u0jglqhh@duck.com>
2024-08-31 10:30:53 +0800

    对齐电池图标

--- tailor.ps1
+++ tailor.ps1
@@ -1,10 +1,10 @@
-$start = 3
+$start = 98
 
-for ($i = 0; $i -lt 20; $i++) { 
+for ($i = 0; $i -lt 9; $i++) { 
     $n = ($start + $i).ToString("D4")
     $name = "$n.png"
     echo $name
-    Magick $name -bordercolor none -border 0x1 $name
+    # Magick $name -bordercolor none -border 1x1 $name
     Magick $name -gravity South -chop 0x1 $name
-    # Magick $name -gravity East  -chop 1x0 $name
+    Magick $name -gravity East  -chop 1x0 $name
 }
